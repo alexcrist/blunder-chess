1c4b93f74793834212696f830d30850a25bcdb0d
alexcrist <alexecrist@gmail.com>
2025-02-10 19:48:51 -0800

    Update title text

--- web-frontend/src/main/Menu/Menu.jsx
+++ web-frontend/src/main/Menu/Menu.jsx
@@ -1,5 +1,7 @@
+import { useCallback, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import chessSlice from "../../chess/chessSlice";
+import { useElementLayoutObserver } from "../../util/useElementLayoutObserver";
 import CheckboardBackground from "../CheckboardBackground/CheckboardBackground";
 import mainSlice from "../mainSlice";
 import styles from "./Menu.module.css";
@@ -14,14 +16,32 @@ const Menu = () => {
     const onPlayOnline = () => {
         dispatch(mainSlice.actions.setIsConnectingToPeer(true));
     };
+
+    const [fontSize, setFontSize] = useState(20);
+    const ref = useRef(null);
+    const onLayout = useCallback(({ width }) => {
+        setFontSize(width / 12);
+    }, []);
+    useElementLayoutObserver(ref, onLayout);
+
     return (
-        <div className={styles.container}>
+        <div className={styles.container} ref={ref}>
             <CheckboardBackground />
-            <div className={styles.title}>blunderchess.net</div>
-            <div className={styles.button} onClick={onPlayLocal}>
+            <div className={styles.title} style={{ fontSize }}>
+                blunderchess.net
+            </div>
+            <div
+                className={styles.button}
+                onClick={onPlayLocal}
+                style={{ fontSize: fontSize * 0.65 }}
+            >
                 Blunder locally
             </div>
-            <div className={styles.button} onClick={onPlayOnline}>
+            <div
+                className={styles.button}
+                onClick={onPlayOnline}
+                style={{ fontSize: fontSize * 0.65 }}
+            >
                 Blunder online
             </div>
         </div>
