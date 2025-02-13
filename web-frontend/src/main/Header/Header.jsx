dd6faa83e6dd6447d7236deaeff02f51aa665967
alexcrist <alexecrist@gmail.com>
2025-02-12 20:12:30 -0800

    Fix link

--- web-frontend/src/main/Header/Header.jsx
+++ web-frontend/src/main/Header/Header.jsx
@@ -1,17 +1,16 @@
 import classNames from "classnames";
-import { useNavigateToMenu } from "../useNavigation";
 import styles from "./Header.module.css";
 
 const Header = ({ isLight }) => {
-    const navigateToMenu = useNavigateToMenu();
     return (
         <div
             className={classNames(styles.container, {
                 [styles.isLight]: isLight,
             })}
-            onClick={navigateToMenu}
         >
-            <a href="/">blunderchess.net</a>
+            <a href="/" target="_">
+                blunderchess.net
+            </a>
         </div>
     );
 };
